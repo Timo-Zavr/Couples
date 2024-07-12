47fd39bed5bdeb04e4a39492664eb86b658a11c0
TimoZavr <zaraisky.timaeus@gmail.com>
2024-07-12 15:59:35 +0300

    Ура!!!

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const start = document.querySelector('.start')
 const inp = document.querySelector('.inp')
 const gameBoard = document.querySelector('.game')
 
+start.addEventListener('click', (event) =>{
+  event.preventDefault()
+  let columns = inp.value
+  let count;
+  if(columns >= 2 && columns <= 6 && columns%2 == 0){
+    count = columns*columns
+  } else{
+      alert("нужно указать чётное число в указанном диапазоне")
+      return
+  }
+  createBoard(count, columns)
+})
 
-let number;
-let count;
-start.addEventListener('click', createBoard)
-
-function createBoard(columns, count){
-    number = parseInt(inp.value)
-    if(number <= 6 && number%2 == 0){
-      count = number*number
-    } else{
-        inp.value = 4
-    }
+function createBoard(count, columns){
 
     gameBoard.textContent = "";
 
@@ -25,15 +27,16 @@ function createBoard(columns, count){
   // В шаблоне находится кнопка "Рестарт"
   const restartBtn = template.querySelector(".table__button");
 
+  for (let i = 0; i < count; i++) {
+    gameTable.append(createCard());
+  }
+
   // Добавляются правила для grid-контейнера в зависимости от значения параметра columns
   gameTable.style = `
   grid-template-columns: repeat(${columns}, 1fr);
   grid-template-rows: repeat(${columns}, 1fr);
   `;
 
-  for (let i = 0; i < count; i++) {
-    gameTable.append(createCard());
-  }
 
   // Получившаяся таблица добавляется в игровое поле
   gameBoard.append(gameTable);
@@ -49,12 +52,10 @@ function createBoard(columns, count){
 
 }
 
-function createCard(){
+function createCard(flippedIcon){
   const template = document.querySelector('#cardTemplate').cloneNode(true).content;
   const gameCard = template.querySelector('.card');
   gameCard.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`)
-  gameCard.style = `
-  grid-template-columns: repeat(${columns}, 1fr);
-  grid-template-rows: repeat(${columns}, 1fr);
-  `
+  return gameCard
+
 }
