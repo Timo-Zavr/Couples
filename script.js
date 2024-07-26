8c8090f80819b61a721b09122360d07da0a1ee40
TimoZavr <zaraisky.timaeus@gmail.com>
2024-07-26 21:01:25 +0300

    4 стихии, спойлер: есть 5

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ import { createBoard } from "./scripts/createBoard.js";
 const start = document.querySelector('.start')
 const inp = document.querySelector('.inp')
 
-const root = document.querySelector(':root')
+const styledElement = document.getElementById('css');
 const Rbut = document.querySelector(".red")
 const Gbut = document.querySelector(".green")
 const Bbut = document.querySelector(".blue")
 const Ybut = document.querySelector(".yellow")
 
 Rbut.addEventListener('click', (event) => {
-    event.preventDefault()
-    root.style.backgroundImage = `url(./Red.jpg)`
+  event.preventDefault()
+  styledElement.href = './red.css'
 })
 Gbut.addEventListener('click', (event) => {
-    event.preventDefault()
-    root.style.backgroundImage = `url(./Green.jpg)`
+  event.preventDefault()
+  styledElement.href = './green.css'
+})
+Bbut.addEventListener('click', (event) => {
+  event.preventDefault()
+  styledElement.href = './blue.css'
+})
+Ybut.addEventListener('click', (event) => {
+  event.preventDefault()
+  styledElement.href = './yellow.css'
 })
 
 start.addEventListener('click', (event) =>{
